7366e5acc9137a87dd510a4a622ba55c7894d55b
Martha Maloba <marthamarumo92@gmail.com>
2025-07-24 22:22:06 +0200

    Update Card.tsx

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,7 +1,7 @@
 // components/common/Card.tsx
 
 import React from "react";
-import type { CardProps } from "@/interfaces";
+import { type CardProps } from "@/interfaces";
 
 const Card: React.FC<CardProps> = ({ title, content }) => {
   return (
