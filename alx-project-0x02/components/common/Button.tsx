a78ea2aaad7ef153e6783d199e54e847571c0d42
Martha Maloba <marthamarumo92@gmail.com>
2025-07-25 09:05:01 +0200

    Update Buttron.tsx

--- alx-project-0x02/components/common/Button.tsx
+++ alx-project-0x02/components/common/Button.tsx
@@ -1,3 +1,4 @@
+
 // pages/about.tsx
 
 import React from "react";
@@ -5,12 +6,12 @@ import Button from "@/components/common/Button";
 
 const About: React.FC = () => {
   return (
-    <div className="max-w-4xl mx-auto p-6">
+    <div className="max-w-3xl mx-auto p-6">
       <h1 className="text-3xl font-bold mb-4">About Us</h1>
-      <p className="mb-6">
-        Welcome to our site! We are committed to providing the best experience.
+      <p className="mb-6 text-lg">
+        We are a company dedicated to delivering excellent service and innovative solutions.
       </p>
-      <Button onClick={() => alert("Thanks for clicking!")}>Click Me</Button>
+      <Button onClick={() => alert("Button clicked!")}>Learn More</Button>
     </div>
   );
 };
