7549fcb80e07706f790d599b0ea66f09105d9972
Martha Maloba <marthamarumo92@gmail.com>
2025-07-25 10:07:02 +0200

    Modify index.ts

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -1,3 +1,4 @@
+// ButtonProps interface
 export interface ButtonProps {
   size?: "small" | "medium" | "large";
   shape?: "rounded-sm" | "rounded-md" | "rounded-lg";
@@ -5,4 +6,10 @@ export interface ButtonProps {
   children?: React.ReactNode;
   disabled?: boolean;
   className?: string;
+}
+
+// CardProps interface
+export interface CardProps {
+  title: string;
+  content: string;
 }
