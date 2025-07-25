98b2cc224b71cde47552f3f9186839d7443bfa38
Martha Maloba <marthamarumo92@gmail.com>
2025-07-25 09:34:09 +0200

    Modify index.ts

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -1,11 +1,8 @@
-
 // interfaces/index.ts
 
-import React from "react";
-
-export interface ButtonProps {
-  size?: "small" | "medium" | "large";
-  shape?: "rounded-sm" | "rounded-md";
-  onClick?: () => void;
-  children: React.ReactNode;
+export interface PostProps {
+  userId: number;
+  id: number;
+  title: string;
+  content: string;
 }
